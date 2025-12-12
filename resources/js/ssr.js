7736c34b98669f2d4ec306a135dfa13430f3d3e1
InsafNilam <insafnilam.2000@gmail.com>
2025-12-12 21:02:14 +0530

    Add SSR setup with Inertia.js and Svelte

---
+++ resources/js/ssr.js
@@ -0,0 +1,18 @@
+import '../css/app.css'
+import { createInertiaApp } from '@inertiajs/svelte'
+import { hydrate, mount } from 'svelte'
+
+createInertiaApp({
+  resolve: (name) => {
+    const pages = import.meta.glob('./Pages/**/*.svelte', { eager: true })
+    return pages[`./Pages/${name}.svelte`]
+  },
+  setup({ el, App, props }) {
+    mount(App, { target: el, props })
+    if (el.dataset.serverRendered === 'true') {
+      hydrate(App, { target: el, props })
+    } else {
+      mount(App, { target: el, props })
+    }
+  },
+})
